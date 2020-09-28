735dde0dd69bd21fe097756e83973c04ffe9cdbd
24wings <731502029@qq.com>
2020-09-29 00:36:26 +0800

    新增company 接口

---
+++ wings.website/Shared/Models/Common/Paged.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace wings.website.Shared.Models.Common
+{
+   public class Paged<T>
+    {
+        public List<T> data { get; set; }
+        public long total { get; set; }
+    }
+}
---
+++ wings.website/Shared/Models/Developer/CompanyQuery.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace wings.website.Shared.Models.Developer
+{
+   public  class CompanyQuery
+    {
+        public int pageIndex { get; set; }
+        public int pageSize { get; set; }
+       public string keywords { get; set; }
+    }
+}
